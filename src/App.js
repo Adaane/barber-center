7e1261fa18bc87af03f0b693e51c60b14dd7d62f
Adaane <adaane.ahamada@hetic.net>
2020-09-14 16:16:09 +0200

    intall firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
 
 function App() {
+
+  useEffect(() => {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDZq-KMxxSUiu488oeaO8ym_3QBNljHClU",
+      authDomain: "barber-center-5f3c7.firebaseapp.com",
+      databaseURL: "https://barber-center-5f3c7.firebaseio.com",
+      projectId: "barber-center-5f3c7",
+      storageBucket: "barber-center-5f3c7.appspot.com",
+      messagingSenderId: "969380377435",
+      appId: "1:969380377435:web:20fba16fe9a66e0e50a07e",
+      measurementId: "G-P2KXQNCDPL"
+    })
+
+    return () => {
+      
+    }
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
