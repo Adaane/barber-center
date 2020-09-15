cc6e0194b667d07433abf501732d10f7d3288b36
Adaane <adaane.ahamada@hetic.net>
2020-09-15 15:21:12 +0200

    create webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path'); //module qui permet de ne pas ecrire le path en entier
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "js/site.js",
+        path: path.resolve( __dirname ,'public')
+    }
+}
